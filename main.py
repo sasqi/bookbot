7da46b6215b36283744acaa5568cef672ec76284
sasqi <stephane@sasqi.email>
2025-05-12 09:54:30 +1000

    attempt at ch2 l4

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+def number_of_words(text):
+    words = text.split()
+    num = 0
+    for n in words:
+        num += 1
+    return num 
+
+
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    num_words = number_of_words(get_book_text("./books/frankenstein.txt"))
+    print(f'{num_words} words found in the document')
 
 
 main()
