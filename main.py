147d1bb7e9437925be7a53452ebe23ad8e19ad00
sasqi <stephane@sasqi.email>
2025-05-12 11:25:07 +1000

    my solution for CH3 L1

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from stats import number_of_words, count_chars
+from stats import number_of_words, count_chars, sort_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
-
+def display_char_rows(dict):
+    for i in dict:
+        if i['char'].isalpha():
+            print(f'{i['char']}: {i['num']}')
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
     num_words = number_of_words(book_text)
-    print(f'{num_words} words found in the document')
-    print(count_chars(book_text))
-
+    chars = sort_chars(count_chars(book_text))
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at ./books/frankenstein.txt ...')
+    print('----------- Word Count ----------')
+    print(f'Found {num_words} total words')
+    print(f'--------- Character Count -------')
+    display_char_rows(chars)
+    print(f'============= END ===============')
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,13 @@ def count_chars(text):
         else:
             chars[c] = 1
     return chars
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_chars(chars):
+    output = []
+    for k,v in chars.items():
+        output.append({'char': k, 'num': v})
+    output.sort(reverse=True, key=sort_on)
+    return output
