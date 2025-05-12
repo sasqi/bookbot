5a824e7ed137d176baf2c5a7c367ef38a0381ea3
sasqi <stephane@sasqi.email>
2025-05-12 10:43:57 +1000

    add CH 2 L6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import number_of_words
+from stats import number_of_words, count_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,8 +7,10 @@ def get_book_text(filepath):
 
 
 def main():
-    num_words = number_of_words(get_book_text("./books/frankenstein.txt"))
+    book_text = get_book_text("./books/frankenstein.txt")
+    num_words = number_of_words(book_text)
     print(f'{num_words} words found in the document')
+    print(count_chars(book_text))
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def number_of_words(text):
     words = text.split()
     return len(words)
+
+def count_chars(text):
+    chars = {}
+    for c in text.lower():
+        if c in chars:
+            chars[c] += 1
+        else:
+            chars[c] = 1
+    return chars
