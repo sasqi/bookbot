6b0c55a827ffe8740d7f2264f840ae5e702e4d25
sasqi <stephane@sasqi.email>
2025-05-12 11:48:30 +1000

    my solution for CH3 L2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 
 def get_book_text(filepath):
@@ -10,17 +11,19 @@ def display_char_rows(dict):
             print(f'{i['char']}: {i['num']}')
 
 def main():
-    book_path = "./books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    num_words = get_num_words(book_text)
-    chars_dict = get_chars_dict(book_text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    print('============ BOOKBOT ============')
-    print(f'Analyzing book found at ./books/frankenstein.txt ...')
-    print('----------- Word Count ----------')
-    print(f'Found {num_words} total words')
-    print(f'--------- Character Count -------')
-    display_char_rows(chars_sorted_list)
-    print(f'============= END ===============')
+    if len(sys.argv) < 2:
+        sys.exit('Usage: python3 main.py <path_to_book>')
+    else:
+        book_text = get_book_text(sys.argv[1])
+        num_words = get_num_words(book_text)
+        chars_dict = get_chars_dict(book_text)
+        chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+        print('============ BOOKBOT ============')
+        print(f'Analyzing book found at ./books/frankenstein.txt ...')
+        print('----------- Word Count ----------')
+        print(f'Found {num_words} total words')
+        print(f'--------- Character Count -------')
+        display_char_rows(chars_sorted_list)
+        print(f'============= END ===============')
 
 main()
